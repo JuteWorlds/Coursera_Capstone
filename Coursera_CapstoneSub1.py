609b56650074433c39a5c0608d9f7a0e2f429577
Saifur Rahman <44307813+SaifurElora@users.noreply.github.com>
2020-04-26 20:38:10 +1000

    CapstoneProject submission1
    This is the first submission of Applied Data Science Capstone of Coursera

---
+++ Coursera_CapstoneSub1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#This note book will be used for a capstone project
+#in data science 
+import pandas as pd
+import numpy as nm
+
+
+# In[4]:
+
+
+print("Hello Capstone Project Course")
+
+
+# In[ ]:
+
+
+
+
